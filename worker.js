e301dc3e0f8c7e0878666fadbea19af21ffafea5
Fathia Olabiyi <fathiatolabiyi02@gmail.com>
2025-09-24 20:12:12 +0100

    update

--- worker.js
+++ worker.js
@@ -1,33 +1,33 @@
-// const { Worker } = require("bullmq");
-// const IORedis = require("ioredis");
-// require('dotenv').config();
-// const sendWelcomeEmail = require("./emailService")
-// const logger = require ('./logger')
+const { Worker } = require("bullmq");
+const IORedis = require("ioredis");
+require('dotenv').config();
+const sendWelcomeEmail = require("./emailService")
+const logger = require ('./logger')
 
 
-// const connection = {
-//   host: "redis-18414.c14.us-east-1-3.ec2.redns.redis-cloud.com",
-//   port: 18414, 
-//   username: "default",
-//   password: process.env.REDIS_PASSWORD,
-// };
+const connection = {
+  host: "redis-18414.c14.us-east-1-3.ec2.redns.redis-cloud.com",
+  port: 18414, 
+  username: "default",
+  password: process.env.REDIS_PASSWORD,
+};
 
 
-// const worker = new Worker ( "email-queue" ,  async (job) => {
-//     console.log(`Processing job ${job.id} for ${job.data.email}`);
-//     console.log('Job data:', job.data);
-//     await sendWelcomeEmail(job.data.email);
-// },
+const worker = new Worker ( "email-queue" ,  async (job) => {
+    console.log(`Processing job ${job.id} for ${job.data.email}`);
+    console.log('Job data:', job.data);
+    await sendWelcomeEmail(job.data.email);
+},
 
-// {connection}
+{connection}
 
-// );
+);
 
-// worker.on("completed" , (job) => {
-//     console.log(`job ${job.id} completed for ${job.data.email}`);
-// });
+worker.on("completed" , (job) => {
+    console.log(`job ${job.id} completed for ${job.data.email}`);
+});
 
-// worker.on("failed" , (job, err) => {
-//     logger.error(err)
-//     console.error(`job ${job.id} failed :  ${err.message}`);
-// });
+worker.on("failed" , (job, err) => {
+    logger.error(err)
+    console.error(`job ${job.id} failed :  ${err.message}`);
+});
