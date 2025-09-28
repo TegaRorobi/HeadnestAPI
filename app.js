8bbbc8edfa7a5ef1273a5649f1a90a4dfc220569
Tega Rorobi <rorobitega@gmail.com>
2025-09-28 23:58:06 +0100

    bugfix: Fix bug with therapyChatRoutes - export router

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.use('/api/journals', journalRoutes);
 app.use('/api', moodRoutes);
 app.use('/api', communityRoutes);
 app.use('/api', therapyRoutes);
-app.use('/api', therapyChatRoutes)
+app.use('/api', therapyChatRoutes);
 app.use("/api", preferencesRoutes);
 app.use('/api', paymentRoutes)
 
